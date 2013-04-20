497fa83adff1c293b6cebbcd645f8e7bc202745a
importre <importre@icloud.com>
2013-04-20 13:21:15 +0900

    cap <-> len

--- 2013/goforjava/slices1.go
+++ 2013/goforjava/slices1.go
@@ -8,10 +8,10 @@ func main() {
 	// START OMIT
 	b := [5]byte{1, 2, 3, 4, 5} // array // HL
 
-	fmt.Printf("b  : %v\ncap: %v\nlen: %v\n\n", b, len(b), cap(b))
+	fmt.Printf("b  : %v\nlen: %v\ncap: %v\n\n", b, len(b), cap(b))
 
 	s := b[2:4] // slice // HL
 
-	fmt.Printf("s  : %v\ncap: %v\nlen: %v\n", s, len(s), cap(s))
+	fmt.Printf("s  : %v\nlen: %v\ncap: %v\n", s, len(s), cap(s))
 	// END OMIT
 }
